830336db0a90ea13f69b3ccc0593d67525d37dac
Aldyfaisal80 <98567736+Aldyfaisal80@users.noreply.github.com>
2024-04-26 21:17:43 +0700

    darkmode berhasil

--- src/js/app.js
+++ src/js/app.js
@@ -28,43 +28,44 @@ window.addEventListener("DOMContentLoaded", () => {
 
     // !!! Dark Mode
 
-    () => {
-        "use strict";
-
-        const storedTheme = localStorage.getItem('theme')
+    (() => {
+        'use strict';
+        const storedTheme = localStorage.getItem('theme');
 
         const getPreferredTheme = () => {
             if (storedTheme) {
-                return storedTheme
+                return storedTheme;
             }
-
-            return window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light'
-        }
+            return window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
+        };
 
         const setTheme = function (theme) {
             if (theme === 'auto' && window.matchMedia('(prefers-color-scheme: dark)').matches) {
-                document.documentElement.setAttribute('data-bs-theme', 'dark')
+                document.documentElement.setAttribute('data-bs-theme', 'dark');
             } else {
-                document.documentElement.setAttribute('data-bs-theme', theme)
+                document.documentElement.setAttribute('data-bs-theme', theme);
             }
-        }
+        };
 
-        setTheme(getPreferredTheme())
+        setTheme(getPreferredTheme());
 
-        const showActiveTheme = theme => {
-            const activeThemeIcon = document.querySelector('.theme-icon-active')
-            const btnToActive = document.querySelector('[data-bs-thene-value="${theme}"]')
-            const iconOfActiveBtn = btnToActive.querySelector('i').dataset.themeIcon
+        const showActiveTheme = (theme, setOpacity = true) => {
+            const activeThemeIcon = document.querySelector('.theme-icon-active');
+            const btnToActive = document.querySelector(`[data-bs-theme-value='${theme}']`);
+            const iconOfActiveBtn = btnToActive.querySelector('i').dataset.themeIcon;
 
             document.querySelectorAll('[data-bs-theme-value]').forEach(element => {
-                element.classList.remove('active')
-            })
+                element.classList.remove('active', 'opacity-75');
+                if (setOpacity) {
+                    element.classList.add('opacity-75');
+                }
+            });
 
-            btnToActive.classList.add('active')
-            activeThemeIcon.classList.remove(activeThemeIcon.dataset.themeIconActive)
-            activeThemeIcon.classList.add(iconOfActiveBtn)
-            activeThemeIcon.dataset.iconActive = iconOfActiveBtn
-        }
+            btnToActive.classList.add('active');
+            activeThemeIcon.classList.remove(activeThemeIcon.dataset.themeIconActive);
+            activeThemeIcon.classList.add(iconOfActiveBtn);
+            activeThemeIcon.dataset.iconActive = iconOfActiveBtn;
+        };
 
         window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', () => {
             if (storedTheme !== 'light' || storedTheme !== 'dark') {
@@ -73,16 +74,15 @@ window.addEventListener("DOMContentLoaded", () => {
         });
 
         window.addEventListener('DOMContentLoaded', () => {
-            showActiveTheme(getPreferredTheme());
+            showActiveTheme(getPreferredTheme(), false);
 
             document.querySelectorAll('[data-bs-theme-value]').forEach((toggle) => {
                 toggle.addEventListener('click', () => {
                     const theme = toggle.getAttribute('data-bs-theme-value');
-                    localStorage.setItem('theme', theme)
+                    localStorage.setItem('theme', theme);
                     setTheme(theme);
-                    showActiveTheme(theme, true);
+                    showActiveTheme(theme, false);
                 });
             });
         });
-    }
-)();
+    })())   
