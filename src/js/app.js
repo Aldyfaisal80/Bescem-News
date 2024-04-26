a5619436704482110c3c470bae1cb320d073d64b
Aldyfaisal80 <98567736+Aldyfaisal80@users.noreply.github.com>
2024-04-26 20:50:03 +0700

    darkmode broken

--- src/js/app.js
+++ src/js/app.js
@@ -1,24 +1,88 @@
-window.addEventListener('DOMContentLoaded', () => {
+window.addEventListener("DOMContentLoaded", () => {
     let scrollPos = 0;
-    const mainNav = document.getElementById('mainNav');
+    const mainNav = document.getElementById("mainNav");
     const headerHeight = mainNav.clientHeight;
-    window.addEventListener('scroll', function() {
+    window.addEventListener("scroll", function () {
         const currentTop = document.body.getBoundingClientRect().top * -1;
-        if ( currentTop < scrollPos) {
+        if (currentTop < scrollPos) {
             // Scrolling Up
-            if (currentTop > 0 && mainNav.classList.contains('is-fixed')) {
-                mainNav.classList.add('is-visible');
+            if (currentTop > 0 && mainNav.classList.contains("is-fixed")) {
+                mainNav.classList.add("is-visible");
             } else {
                 console.log(123);
-                mainNav.classList.remove('is-visible', 'is-fixed');
+                mainNav.classList.remove("is-visible", "is-fixed");
             }
         } else {
             // Scrolling Down
-            mainNav.classList.remove(['is-visible']);
-            if (currentTop > headerHeight && !mainNav.classList.contains('is-fixed')) {
-                mainNav.classList.add('is-fixed');
+            mainNav.classList.remove(["is-visible"]);
+            if (
+                currentTop > headerHeight &&
+                !mainNav.classList.contains("is-fixed")
+            ) {
+                mainNav.classList.add("is-fixed");
             }
         }
         scrollPos = currentTop;
     });
-})
+})(
+
+    // !!! Dark Mode
+
+    () => {
+        "use strict";
+
+        const storedTheme = localStorage.getItem('theme')
+
+        const getPreferredTheme = () => {
+            if (storedTheme) {
+                return storedTheme
+            }
+
+            return window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light'
+        }
+
+        const setTheme = function (theme) {
+            if (theme === 'auto' && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+                document.documentElement.setAttribute('data-bs-theme', 'dark')
+            } else {
+                document.documentElement.setAttribute('data-bs-theme', theme)
+            }
+        }
+
+        setTheme(getPreferredTheme())
+
+        const showActiveTheme = theme => {
+            const activeThemeIcon = document.querySelector('.theme-icon-active')
+            const btnToActive = document.querySelector('[data-bs-thene-value="${theme}"]')
+            const iconOfActiveBtn = btnToActive.querySelector('i').dataset.themeIcon
+
+            document.querySelectorAll('[data-bs-theme-value]').forEach(element => {
+                element.classList.remove('active')
+            })
+
+            btnToActive.classList.add('active')
+            activeThemeIcon.classList.remove(activeThemeIcon.dataset.themeIconActive)
+            activeThemeIcon.classList.add(iconOfActiveBtn)
+            activeThemeIcon.dataset.iconActive = iconOfActiveBtn
+        }
+
+        window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', () => {
+            if (storedTheme !== 'light' || storedTheme !== 'dark') {
+                setTheme(getPreferredTheme());
+            }
+        });
+
+        window.addEventListener('DOMContentLoaded', () => {
+            showActiveTheme(getPreferredTheme());
+
+            document.querySelectorAll('[data-bs-theme-value]').forEach((toggle) => {
+                toggle.addEventListener('click', () => {
+                    const theme = toggle.getAttribute('data-bs-theme-value');
+                    localStorage.setItem('theme', theme)
+                    setTheme(theme);
+                    showActiveTheme(theme, true);
+                });
+            });
+        });
+    }
+)();
