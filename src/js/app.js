3334fd346d67d1578ded068a9d420f81cd85d63a
Aldyfaisal80 <98567736+Aldyfaisal80@users.noreply.github.com>
2024-04-28 21:29:50 +0700

    done

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,5 @@
+const { Tooltip } = require("bootstrap");
+
 window.addEventListener("DOMContentLoaded", () => {
     let scrollPos = 0;
     const mainNav = document.getElementById("mainNav");
@@ -118,3 +120,49 @@ window.addEventListener('DOMContentLoaded', () => {
 
     window.addEventListener('scroll', handleScroll);
 });
+
+
+// Biodata Kelompok
+const cards = document.querySelectorAll('.card-wrap');
+
+cards.forEach(card => {
+  card.addEventListener('mouseenter', () => {
+    const info = card.querySelector('.card-info');
+    const bg = card.querySelector('.card-bg');
+
+    info.style.transform = 'translateY(0)';
+    info.querySelector('p').style.opacity = 1;
+    info.style.transition = '0.6s cubic-bezier(0.23, 1, 0.32, 1)';
+    info.querySelector('p').style.transition = '0.6s cubic-bezier(0.23, 1, 0.32, 1)';
+    info.querySelector(':after').style.transition = '5s cubic-bezier(0.23, 1, 0.32, 1)';
+    info.querySelector(':after').style.opacity = 1;
+    info.querySelector(':after').style.transform = 'translateY(0)';
+
+    bg.style.transition = '0.6s cubic-bezier(0.23, 1, 0.32, 1), opacity 5s cubic-bezier(0.23, 1, 0.32, 1)';
+    bg.style.opacity = 0.8;
+
+    card.querySelector('.card').style.transition = '0.6s cubic-bezier(0.23, 1, 0.32, 1), box-shadow 2s cubic-bezier(0.23, 1, 0.32, 1)';
+    card.querySelector('.card').style.boxShadow = 'rgba(255, 255, 255, 0.2) 0 0 40px 5px, rgba(255, 255, 255, 1) 0 0 0 1px, rgba(0, 0, 0, 0.66) 0 30px 60px 0, inset #333 0 0 0 5px, inset white 0 0 0 6px';
+  });
+
+  card.addEventListener('mouseleave', () => {
+    const info = card.querySelector('.card-info');
+    const bg = card.querySelector('.card-bg');
+
+    info.style.transform = '';
+    info.querySelector('p').style.opacity = '';
+    info.style.transition = '';
+    info.querySelector('p').style.transition = '';
+    info.querySelector(':after').style.transition = '';
+    info.querySelector(':after').style.opacity = '';
+    info.querySelector(':after').style.transform = '';
+
+    bg.style.transition = '';
+    bg.style.opacity = '';
+
+    card.querySelector('.card').style.transition = '';
+    card.querySelector('.card').style.boxShadow = '';
+  });
+});
+
+// ! Tooltip
