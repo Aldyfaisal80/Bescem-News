4e4399d3e7a96874d68a15a0efc13644166d8777
Aldyfaisal80 <98567736+Aldyfaisal80@users.noreply.github.com>
2024-04-28 21:38:25 +0700

    done

--- src/js/app.js
+++ src/js/app.js
@@ -1,124 +1,122 @@
-const { Tooltip } = require("bootstrap");
-
 window.addEventListener("DOMContentLoaded", () => {
-    let scrollPos = 0;
-    const mainNav = document.getElementById("mainNav");
-    const headerHeight = mainNav.clientHeight;
-    window.addEventListener("scroll", function () {
-        const currentTop = document.body.getBoundingClientRect().top * -1;
-        if (currentTop < scrollPos) {
-            // Scrolling Up
-            if (currentTop > 0 && mainNav.classList.contains("is-fixed")) {
-                mainNav.classList.add("is-visible");
-            } else {
-                console.log(123);
-                mainNav.classList.remove("is-visible", "is-fixed");
-            }
-        } else {
-            // Scrolling Down
-            mainNav.classList.remove(["is-visible"]);
-            if (
-                currentTop > headerHeight &&
-                !mainNav.classList.contains("is-fixed")
-            ) {
-                mainNav.classList.add("is-fixed");
-            }
-        }
-        scrollPos = currentTop;
-    });
+  let scrollPos = 0;
+  const mainNav = document.getElementById("mainNav");
+  const headerHeight = mainNav.clientHeight;
+  window.addEventListener("scroll", function () {
+    const currentTop = document.body.getBoundingClientRect().top * -1;
+    if (currentTop < scrollPos) {
+      // Scrolling Up
+      if (currentTop > 0 && mainNav.classList.contains("is-fixed")) {
+        mainNav.classList.add("is-visible");
+      } else {
+        console.log(123);
+        mainNav.classList.remove("is-visible", "is-fixed");
+      }
+    } else {
+      // Scrolling Down
+      mainNav.classList.remove(["is-visible"]);
+      if (
+        currentTop > headerHeight &&
+        !mainNav.classList.contains("is-fixed")
+      ) {
+        mainNav.classList.add("is-fixed");
+      }
+    }
+    scrollPos = currentTop;
+  });
 })(
 
-    // !!! Dark Mode
+  // !!! Dark Mode
 
-    (() => {
-        'use strict';
-        const storedTheme = localStorage.getItem('theme');
+  (() => {
+    'use strict';
+    const storedTheme = localStorage.getItem('theme');
 
-        const getPreferredTheme = () => {
-            if (storedTheme) {
-                return storedTheme;
-            }
-            return window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
-        };
+    const getPreferredTheme = () => {
+      if (storedTheme) {
+        return storedTheme;
+      }
+      return window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
+    };
 
-        const setTheme = function (theme) {
-            if (theme === 'auto' && window.matchMedia('(prefers-color-scheme: dark)').matches) {
-                document.documentElement.setAttribute('data-bs-theme', 'dark');
-            } else {
-                document.documentElement.setAttribute('data-bs-theme', theme);
-            }
-        };
+    const setTheme = function (theme) {
+      if (theme === 'auto' && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+        document.documentElement.setAttribute('data-bs-theme', 'dark');
+      } else {
+        document.documentElement.setAttribute('data-bs-theme', theme);
+      }
+    };
 
+    setTheme(getPreferredTheme());
+
+    const showActiveTheme = (theme, setOpacity = true) => {
+      const activeThemeIcon = document.querySelector('.theme-icon-active');
+      const btnToActive = document.querySelector(`[data-bs-theme-value='${theme}']`);
+      const iconOfActiveBtn = btnToActive.querySelector('i').dataset.themeIcon;
+
+      document.querySelectorAll('[data-bs-theme-value]').forEach(element => {
+        element.classList.remove('active', 'opacity-75');
+        if (setOpacity) {
+          element.classList.add('opacity-75');
+        }
+      });
+
+      btnToActive.classList.add('active');
+      activeThemeIcon.classList.remove(activeThemeIcon.dataset.themeIconActive);
+      activeThemeIcon.classList.add(iconOfActiveBtn);
+      activeThemeIcon.dataset.iconActive = iconOfActiveBtn;
+    };
+
+    window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', () => {
+      if (storedTheme !== 'light' || storedTheme !== 'dark') {
         setTheme(getPreferredTheme());
+      }
+    });
 
-        const showActiveTheme = (theme, setOpacity = true) => {
-            const activeThemeIcon = document.querySelector('.theme-icon-active');
-            const btnToActive = document.querySelector(`[data-bs-theme-value='${theme}']`);
-            const iconOfActiveBtn = btnToActive.querySelector('i').dataset.themeIcon;
+    window.addEventListener('DOMContentLoaded', () => {
+      showActiveTheme(getPreferredTheme(), false);
 
-            document.querySelectorAll('[data-bs-theme-value]').forEach(element => {
-                element.classList.remove('active', 'opacity-75');
-                if (setOpacity) {
-                    element.classList.add('opacity-75');
-                }
-            });
-
-            btnToActive.classList.add('active');
-            activeThemeIcon.classList.remove(activeThemeIcon.dataset.themeIconActive);
-            activeThemeIcon.classList.add(iconOfActiveBtn);
-            activeThemeIcon.dataset.iconActive = iconOfActiveBtn;
-        };
-
-        window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', () => {
-            if (storedTheme !== 'light' || storedTheme !== 'dark') {
-                setTheme(getPreferredTheme());
-            }
+      document.querySelectorAll('[data-bs-theme-value]').forEach((toggle) => {
+        toggle.addEventListener('click', () => {
+          const theme = toggle.getAttribute('data-bs-theme-value');
+          localStorage.setItem('theme', theme);
+          setTheme(theme);
+          showActiveTheme(theme, false);
         });
-
-        window.addEventListener('DOMContentLoaded', () => {
-            showActiveTheme(getPreferredTheme(), false);
-
-            document.querySelectorAll('[data-bs-theme-value]').forEach((toggle) => {
-                toggle.addEventListener('click', () => {
-                    const theme = toggle.getAttribute('data-bs-theme-value');
-                    localStorage.setItem('theme', theme);
-                    setTheme(theme);
-                    showActiveTheme(theme, false);
-                });
-            });
-        });
-    })())
+      });
+    });
+  })())
 
 
 // Animasi Navbar
 window.addEventListener('DOMContentLoaded', () => {
-    let scrollPos = 0;
-    const mainNav = document.getElementById('mainNav');
-    const headerHeight = mainNav.offsetHeight; // Menggunakan offsetHeight untuk mendapatkan tinggi navbar yang akurat
+  let scrollPos = 0;
+  const mainNav = document.getElementById('mainNav');
+  const headerHeight = mainNav.offsetHeight; // Menggunakan offsetHeight untuk mendapatkan tinggi navbar yang akurat
 
-    const handleScroll = () => {
-        const currentTop = window.pageYOffset || document.documentElement.scrollTop; // Mendapatkan posisi scroll saat ini
+  const handleScroll = () => {
+    const currentTop = window.pageYOffset || document.documentElement.scrollTop; // Mendapatkan posisi scroll saat ini
 
-        if (currentTop < scrollPos) {
-            // Scrolling Up
-            if (currentTop > 0 && !mainNav.classList.contains('is-visible')) {
-                mainNav.classList.add('is-visible');
-            } else {
-                mainNav.classList.remove('is-visible', 'is-fixed');
-            }
-        } else {
-            // Scrolling Down
-            mainNav.classList.remove('is-visible');
-            if (currentTop > headerHeight && !mainNav.classList.contains('is-fixed')) {
-                // mainNaw.classList.add(getPreferredTheme())
-                mainNav.classList.add('is-fixed');
-            }
-        }
+    if (currentTop < scrollPos) {
+      // Scrolling Up
+      if (currentTop > 0 && !mainNav.classList.contains('is-visible')) {
+        mainNav.classList.add('is-visible');
+      } else {
+        mainNav.classList.remove('is-visible', 'is-fixed');
+      }
+    } else {
+      // Scrolling Down
+      mainNav.classList.remove('is-visible');
+      if (currentTop > headerHeight && !mainNav.classList.contains('is-fixed')) {
+        // mainNaw.classList.add(getPreferredTheme())
+        mainNav.classList.add('is-fixed');
+      }
+    }
 
-        scrollPos = currentTop;
-    };
+    scrollPos = currentTop;
+  };
 
-    window.addEventListener('scroll', handleScroll);
+  window.addEventListener('scroll', handleScroll);
 });
 
 
@@ -164,5 +162,3 @@ cards.forEach(card => {
     card.querySelector('.card').style.boxShadow = '';
   });
 });
-
-// ! Tooltip
